674228ce423b3d1ac73344227581e3f53367a91e
Benjamin Brieber <bbrieber@cs.uni-bremen.de>
2014-12-08 18:50:00 +0100

    added the knowrob exporter changed some small stuff in the motion capture script

--- xsens_mocap/stream_recorder.py
+++ xsens_mocap/stream_recorder.py
@@ -10,17 +10,13 @@ import signal
 import sys
 #from struct import *
 
-class mvn_listener():
-#(Subject, threading.Thread):
-    
+class MvnListener():
 
-    def __init__(self,output, ip='127.0.0.1',port=9763):
+    def __init__(self,ip='127.0.0.1',port=9763):
         #Subject.__init__(self)
         #threading.Thread.__init_(self)
         self._udp_ip = ip
         self._udp_port = port
-        self._output = output
-        self.count = 0
 
 
     def connect(self):
@@ -29,60 +25,80 @@ class mvn_listener():
         self._sock.bind((self._udp_ip,self._udp_port))
         print('done')
 
-#    def run(self):
-#
 
+    
     def read_data(self):
-        self.count = self.count + 1
         recv_data = self._sock.recv(4096)
         header = xsens_header(recv_data)
         if header.isQuaternion():
-            if len(recv_data) != 760:
-                print("wtf")
-            self._output.write(recv_data)
+            self.handle_quaternion(recv_data)
+        elif header.isEuler():
+            self.handle_euler(recv_data)
+        elif header.isPointData():
+            self.handle_pointData(recv_data)
+        elif header.isMotionGridData():
+            self.handle_motionGridData(recv_data)
+        elif header.isScaleInfo():
+            self.handle_scaleInfo(recv_data)
+        elif header.isPropInfo():
+            self.handle_propInfo(recv_data)
+        elif header.isMetaData():
+            self.handle_metaData(recv_data)
         else:
             print("FATAL ERROR: this should never happen\n continue anyway :P")
-        #print(self.count)
-        #print(len(recv_data))
-        #if len(recv_data) != 760:
-        #    print('fuck')
-        #    print(len(recv_data))
+
 
     def handle_quaternion(self,data):
         return
+
 
     def handle_euler(self,data):
         return
 
+
     def handle_pointData(self,data):
         return
+
 
     def handle_motionGridData(self,data):
         return
 
+
     def handle_scaleInfo(self,data):
         return
+
 
     def handle_propInfo(self,data):
         return
 
+
     def handle_metaData(self,data):
         return
-
-    def handle_quaternion(self,data):
-        return
-
 
 
     def clean_up(self):
         print("count: "+self.count)
+
+class MvnFilewriter(MvnListener):
+
+    def __init__(self,output, ip='127.0.0.1',port=9763):
+        MvnListener.__init__(self,ip,port)
+        self._output = output
+
+
+    
+    def handle_quaternion(self,data):
+        if len(data) != 760:
+            print("wtf")
+        self._output.write(data)
+        
 
 running = 1
 
 
 def main():
     with open("xsens.xcap",'wb') as output:
-        client = mvn_listener(output=output, ip='192.168.100.212',port=9763)
+        client = MvnFilewriter(output=output, ip='192.168.100.212',port=9763)
         client.connect()
         while 1:
             client.read_data()
