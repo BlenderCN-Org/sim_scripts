d9bfe68eaaa2f86394fb315eceb687d2fb34b153
Benjamin Brieber <bbrieber@cs.uni-bremen.de>
2014-12-09 18:41:55 +0100

    skeleton blender config...

--- blender_xsens/skeleton_mod.py
+++ blender_xsens/skeleton_mod.py
@@ -12,14 +12,17 @@ class SkeletonModifier():
         scene.frame_start = 1
         scene.frame_end = 1
         self.current_frame = 1
-        self.skeleton = slef.scene.objects.active
+        self.skeleton = self.scene.objects.active
         
     def update_skeleton(self):
         self.scene.frame_set(frame = self.current_frame)
         self.current_frame += 1
+        bone = self.skeleton.pose.bones['Hips']
+        bone.location[2] = 2
+        
+        
+        bpy.ops.pose.select_all(action='SELECT')	#DESELECT,TOGGLE
         bpy.ops.anim.keyframe_insert_menu(type='Rotation')
         bpy.ops.anim.keyframe_insert_menu(type='Location')
         bpy.ops.anim.keyframe_insert_menu(type='Scale')
-        bone=self.skeleton.bones['Hips']
-        
         
