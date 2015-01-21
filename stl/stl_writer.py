5a6dbf8e82cbfd681f7d9944165788642b3ad064
Benjamin Brieber <bbrieber@cs.uni-bremen.de>
2015-01-21 15:12:40 +0100

    added a heightmap 2 stl converter and an example image

--- stl/stl_writer.py
+++ stl/stl_writer.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 #coding:utf-8
 # Purpose: Export 3D objects, build of faces with 3 or 4 vertices, as ASCII or Binary STL file.
@@ -82,33 +83,86 @@ class Binary_STL_Writer(ASCII_STL_Writer):
 
 
 def example():
-    def get_cube():
-        # cube corner points
-        s = 3.
-        p1 = (0, 0, 0)
-        p2 = (0, 0, s)
-        p3 = (0, s, 0)
-        p4 = (0, s, s)
-        p5 = (s, 0, 0)
-        p6 = (s, 0, s)
-        p7 = (s, s, 0)
-        p8 = (s, s, s)
-
-        # define the 6 cube faces
-        # faces just lists of 3 or 4 vertices
-        return [
-            [p1, p5, p7, p3],
-            [p1, p5, p6, p2],
-            [p5, p7, p8, p6],
-            [p7, p8, p4, p3],
-            [p1, p3, p4, p2],
-            [p2, p6, p8, p4],
-        ]
+    
+    # cube corner points
+    s = 3.
+    p1 = (0, 0, 0)
+    p2 = (0, 0, s)
+    p3 = (0, s, 0)
+    p4 = (0, s, s)
+    p5 = (s, 0, 0)
+    p6 = (s, 0, s)
+    p7 = (s, s, 0)
+    p8 = (s, s, s)
+    
+    # define the 6 cube faces
+    # faces just lists of 3 or 4 vertices
+    return [
+        [p1, p5, p7, p3],
+        [p1, p5, p6, p2],
+        [p5, p7, p8, p6],
+        [p7, p8, p4, p3],
+        [p1, p3, p4, p2],
+        [p2, p6, p8, p4],
+    ]
 
     with open('cube.stl', 'wb') as fp:
         writer = Binary_STL_Writer(fp)
         writer.add_faces(get_cube())
         writer.close()
 
+
+import Image
+
+def read_image(filename, dimensions ,offset=(0.,0.,0.)):
+    im = Image.open(filename)
+    sx,sy = im.size
+    max = 0
+    min = 255
+    shift_x = -1 * (dimensions[0] / 2) + offset[0]
+    shift_y = -1 * (dimensions[1] / 2) + offset[1]
+    shift_z = offset[2]
+    height_fac = 1. / 255.
+    x_fac = 1. / sx
+    y_fac = 1. / sy
+    points = list()
+    for dx in range(sx):
+        c_points = list()
+        for dy in range(sy):
+            dz  = im.getpixel((dx,dy))
+            cx = dx * x_fac * dimensions[0] + shift_y
+            cy = dy * y_fac * dimensions[1] + shift_y
+            cz = dz * height_fac *dimensions[2] + shift_z
+            c_points.append(list((cx,cy,cz)))
+            #if size > max:
+            #    max = size
+            #if size < min:
+            #    min = size
+        points.append(c_points)
+        
+    faces = list()
+    for dx in range(sx-1):
+        for dy in range(sy-1):
+            faces.append(list((
+                points[dx][dy],
+                points[dx][dy+1],
+                points[dx+1][dy]
+            )))
+            faces.append(list((
+                points[dx][dy+1],
+                points[dx+1][dy+1],
+                points[dx+1][dy]
+            )))
+            
+    print(max)
+    print(min)
+    with open('map.stl', 'wb') as fp:
+        writer = Binary_STL_Writer(fp)
+        writer.add_faces(faces)
+        writer.close()
+#    return faces
+
+
+    
 if __name__ == '__main__':
     example()
